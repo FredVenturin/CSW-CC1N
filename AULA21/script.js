8772422f936e75da331d83492b38212e08e56941
FredVenturin <frederico.venturin@gmail.com>
2025-05-19 20:39:07 -0300

    Add files via upload

---
+++ AULA21/script.js
@@ -0,0 +1,12 @@
+
+
+function calculadoraIdade () {
+    var idadePessoa =  document.getElementById("idade").value;
+    if (1 >= idadePessoa <= 11) {
+    window.alert ("Você é uma criança!");
+    } else {
+        window.alert ("viado")
+    }
+
+    
+}
