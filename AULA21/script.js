2cdd530970256afc2d66780ef3ed02d8ee4ee152
FredVenturin <frederico.venturin@gmail.com>
2025-05-21 19:37:52 -0300

    Add files via upload

--- AULA21/script.js
+++ AULA21/script.js
@@ -3,7 +3,18 @@ function calculadoraIdade() {
 
     if (idadePessoa >= 1 && idadePessoa <= 11) {
         window.alert("Você é uma criança!");
-    } else {
-        window.alert("Você não é uma criança.");
+    } 
+    else if (idadePessoa > 11 && idadePessoa <= 15) {
+        window.alert("Você é um adolescente!");
     }
+    else if (idadePessoa > 15 && idadePessoa <= 29) {
+        window.alert("Você é um jovem adulto!");
+    }
+    else if (idadePessoa > 29 && idadePessoa <= 60) {
+        window.alert("Você é um adulto!");
+    }
+    else { 
+        window.alert("Você é um idoso!");
+    }
+    
 }
