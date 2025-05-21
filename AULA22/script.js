adf14e11a3e76ff740dcb975147692b7a66a65d5
FredVenturin <frederico.venturin@gmail.com>
2025-05-21 20:41:51 -0300

    Add files via upload

---
+++ AULA22/script.js
@@ -0,0 +1,16 @@
+
+
+for (let par = 0; par <= 1000; par += 2)
+{
+    let impar = 999 - par
+   
+    document.write("<h2>" + par + "</h2><br>"+"<h2>" + impar  + "</h2><br>");
+
+    console.log(impar);
+    console.log(par);
+}
+
+
+
+
+  
---
+++ AULA22/script2.js
@@ -0,0 +1,8 @@
+let dia = document.getElementById ("dia");
+let ano = document.getElementById ("ano");
+let select = document.getElementById ("meses");
+let 
+
+function calculadoraIdade() {
+
+}
