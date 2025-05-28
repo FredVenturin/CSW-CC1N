7fce00859f6205d664e1c4183f44ce67ec091c95
FredVenturin <frederico.venturin@gmail.com>
2025-05-28 20:09:14 -0300

    Add files via upload

---
+++ AULA25/script.js
@@ -0,0 +1,18 @@
+function calculadora() {
+    let dia = Number(document.getElementById("dia").value);
+    const mes = document.getElementById("mes").value;
+    let ano = Number(document.getElementById("ano").value);
+
+    let datanasc = new Date(mes + '/' + dia + '/' + ano);
+    let dataatual = new Date();
+    let resultado = dataatual.getFullYear() - datanasc.getFullYear();
+
+
+    if ( dia < dataatual.getDate() && mes > dataatual.getMonth() ) {
+        resultado--
+    }
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado+ " anos.";
+
+
+}
